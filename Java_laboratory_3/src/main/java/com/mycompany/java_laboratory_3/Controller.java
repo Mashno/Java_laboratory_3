e8b434f366b15e11f8f1c85625dc9bd4e3b15eea
Mashno <118561914+Mashno@users.noreply.github.com>
2025-04-17 17:22:05 +0300

    commit
    сделал базовую гуи

--- Java_laboratory_3/src/main/java/com/mycompany/java_laboratory_3/Controller.java
+++ Java_laboratory_3/src/main/java/com/mycompany/java_laboratory_3/Controller.java
@@ -1,16 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.java_laboratory_3;
 
 /**
  *
  * @author Владислав
  */
-public class Java_laboratory_3 {
-
-    public static void main(String[] args) {
-        new Controller();
+public class Controller {
+    
+    
+    public Controller(){
+        new View(this).setVisible(true);
     }
 }
