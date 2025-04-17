a1bf608f5ff034a4f444073947226ce287d94aee
Mashno <118561914+Mashno@users.noreply.github.com>
2025-04-17 15:53:29 +0300

    commit

---
+++ Java_laboratory_3/src/main/java/com/mycompany/java_laboratory_3/Java_laboratory_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_laboratory_3;
+
+/**
+ *
+ * @author Владислав
+ */
+public class Java_laboratory_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
